25c5ec6f4993a58459df54df8ae69a75afab550f
Sylvester Obeng <devxprogramming@gmail.com>
2024-02-26 04:20:50 +0000

    Changes to codebase

---
+++ staticfiles/assets/js/custom.js
@@ -0,0 +1,15 @@
+function goBack() {
+    window.history.back()
+}
+
+
+var alert = document.querySelector('.alert');
+
+setTimeout(function() {
+  alert.classList.add('fade'); 
+  alert.classList.remove('show');
+  
+  // Trigger resize to adjust content
+  window.dispatchEvent(new Event('resize'));
+  
+}, 2500);
